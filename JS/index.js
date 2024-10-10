f4d14068c9c28a30fb5ed7e566b6b9415be486f0
Wanderson Silva Freitas <sor.vanderson@gmail.com>
2024-10-09 23:09:49 -0300

    Corrigido a exibição do alerta quando se é enviado os inputs

--- JS/index.js
+++ JS/index.js
@@ -1,5 +1,4 @@
 function exibirAlerta() {
     var nome = window.document.getElementById("nome").value;
     alert("Olá, " + nome + "! Entraremos em contato em breve!"); 
-    return false; //Impede o envio do formulário
 }
